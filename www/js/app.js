91c34e89bb6e4299106f01c01b35691b42d5e796
Nghia Tran <m18tran@students.latrobe.edu.au>
2015-05-07 16:30:42 +1000

    Alpha release

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic','ngMap'])
+angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -19,44 +19,62 @@ angular.module('starter', ['ionic','ngMap'])
 })
 
 .controller('MapController', function($scope, $ionicLoading) {
-    var posOptions = {timeout: 10000, enableHighAccuracy: true};
-    var myLatlng = new google.maps.LatLng(37.3000, -120.4833),
+
+    var posOptions = {timeout: 2000, enableHighAccuracy: true}; // map option, set time out 100 msand high accuracy
+
+    var myLatlng = new google.maps.LatLng(37.3000, -120.4833), // random initial location
         mapOptions = {
             center: myLatlng,
             zoom: 16,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         },
-    map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    $scope.data = {"success":true,"ErrCode":0,"lat":0,"lng":0,timeStamp:new Date()};
 
-   $scope.getLocation = function()
+    map = new google.maps.Map(document.getElementById("map"), mapOptions); // initialize the map
+    $scope.data = {"success":true,"ErrCode":0,"lat":0,"lng":0,timeStamp:new Date()}; // initialize data object to send
+    var myLocation = new  google.maps.Marker()
+   $scope.toggleTracking = function()
    {
-      navigator.geolocation.getCurrentPosition(updateLocationData,errorOccured,posOptions);
-      console.log($scope.data);
+      /*navigator.geolocation.getCurrentPosition(updateLocationData,errorOccured,posOptions);
+      console.log($scope.data);*/
+      if ($scope.watchID) {
+        navigator.geolocation.clearWatch($scope.watchID); // unwatch the 
+        $scope.watchID=null;
+
+        $scope.data={"success":false,"ErrCode":3,"lat":0,"lng":0,"timeStamp":new Date()};
+
+        myLocation.setMap(null); // remove marker from map
+      }else{
+        $scope.watchID = navigator.geolocation.watchPosition(updateLocationData, errorOccured, posOptions);
+      }
+
       return $scope;
    }
    // initialize the map
    google.maps.event.addDomListener(window, 'load', function() {
         
-        $scope.getLocation().$apply();
+        $scope.watchID = navigator.geolocation.watchPosition(updateLocationData, errorOccured, posOptions);
     
         $scope.map = map;
     });
 
    // this call backfunction is used to update data object 
    var updateLocationData= function(pos){
+      // update the data json info
       $scope.data.success= true;
       $scope.data.ErrCode = 0;
       $scope.data.lat = pos.coords.latitude;
       $scope.data.lng = pos.coords.longitude;
       $scope.data.timeStamp = new Date();
+      // update display map
       map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      // marker here
-      var myLocation = new google.maps.Marker({
+      // update marker
+      myLocation.setMap(null); // unset the old one
+      myLocation = new google.maps.Marker({ 
           position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
           map: map,
-          title: "My Location"
+          title: "Current Location"
       });
+      $scope.$apply()
    }
 
 
@@ -68,6 +86,6 @@ angular.module('starter', ['ionic','ngMap'])
       $scope.data.lng = "0";
       $scope.data.timeStamp = new Date();
       $scope.data.error
+      $scope.$apply()
    }
-
 });
